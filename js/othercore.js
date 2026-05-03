d0de64bca74231950ae5756fb57eda7c66aabe6b
iiZiad <ziadwa621@gmail.com>
2026-05-03 18:44:10 +0300

    Delete js/othercore.js

--- js/othercore.js
+++
@@ -1,7 +0,0 @@
-let fileCss = document.getElementById("theme");
-function lightTheme(){
-    fileCss.setAttribute('href','');
-}
-function darkTheme(){
-    fileCss.setAttribute('href','../css/darktheme.css');
-}
